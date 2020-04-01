064e3185d56d8240dfa77cc8e7d83f5366efa089
Nixon Mathew <nixon.nelson@solvezy.com>
2020-04-02 03:56:52 +0530

    db connectivity issues resolved

--- src/main/java/com/solv/starter/UserController.java
+++ src/main/java/com/solv/starter/UserController.java
@@ -2,17 +2,11 @@ package com.solv.starter;
 
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
---
+++ src/main/resources/data-postgres.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO users(name,mobile,address)
+VALUES ('Ruby','9457688756','Mullenkuzhiyil Mathew Vihar');
